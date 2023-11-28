9ec1a462ceed6a217b4d022d6b617ed0bc4b266a
Marina <khamikova.ma@dmail.com>
2023-11-28 11:21:20 +0300

    Initial commit

---
+++ matrix.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include "matrix.h"
+
+using namespace std;
+
+Matrix::Matrix (int size1, int size2, Vector* rows) {
+    this->size1 = size1;
+    this->size2 = size2;
+
+    this->rows = reinterpret_cast<Vector*>(new int[size1 * sizeof(Vector)]);
+
+    for (int i = 0; i < size1; ++i) {
+        this->rows[i] = rows[i];
+    }
+
+    cout << " Matrix initialisation: size " << this->size1 << ", " << this->size2 << endl;
+}
+
+Matrix::~Matrix () {
+    delete[] this->rows;
+}
+
+int Matrix::get_size1() const {
+    return size1;
+}
+
+int Matrix::get_size2() const {
+    return size2;
+}
+
+double Matrix::get_value(int i, int j) const {
+    return this->rows[i].get_value(j);
+}
+
+void Matrix::print_values() const {
+    for (int i = 0; i < size1; ++i) {
+        rows[i].print_values();
+//        cout << i << '\n';
+    }
+}
+
+Vector Matrix::mult_vector(const Vector& a) const {
+    Vector b(size1);
+    for (int i = 0; i < size1; ++i) {
+        b.set_value(i, rows[i].scalar_product(a));
+    }
+    return b;
+}
